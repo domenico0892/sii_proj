96cbddfb5e7c5337865856dee62c37607580a42a
Domenico Giammarino <giammarinodomenico@gmail.com>
2016-01-29 14:15:41 +0100

    fixed prompt js

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,25 +1,26 @@
 $(document).ready(function() {
-var myExampleClickHandler = function(event){ 
-    console.log(this.label);
-    var retVal = prompt("Nome del campo: ","");
-    $.ajax({
-        url : "PatternController",
-        type: "get",
-        data: {name:retVal, newPattern: this.label},
-        dataType: "text",                   
-        success: function (data){
-        	console.log("OK", data);
-        },
-        error: function (data){
-        	console.log("NO", data);
-        }
+	var myExampleClickHandler = function(event){ 
+		console.log(this.label);
+		var retVal = prompt("Nome del campo: ","");
+		if (retVal != undefined && retVal != "") {
+			$.ajax({
+				url : "PatternController",
+				type: "get",
+				data: {name:retVal, newPattern: this.label},
+				dataType: "text",                   
+				success: function (data){
+					console.log("OK", data);
+				},
+				error: function (data){
+					console.log("NO", data);
+				}
+			});}
+	};
+	var myDomOutline = DomOutline({ 
+		onClick: myExampleClickHandler, 
+		filter: false,
+		stopOnClick: false,
+		hideLabel: false
 	});
-};
-  var myDomOutline = DomOutline({ 
-    onClick: myExampleClickHandler, 
-    filter: false,
-    stopOnClick: false,
-    hideLabel: false
-  });
-  myDomOutline.start();
+	myDomOutline.start();
 });
